3c31a64136a382c75658c8200ed71500a6ef4370
coltynw <coltynw@users.github.com>
2020-05-15 13:37:23 -0400

    done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.store = [None] * capacity
+        self.current = 0
 
     def append(self, item):
-        pass
+        self.store[self.current] = item
+        self.advance()
+
+    def advance(self):
+        if self.current == self.capacity - 1:
+            self.current = 0
+        else:
+            self.current += 1
 
     def get(self):
-        pass
+        buffer = [item for item in self.store if item is not None]
+        return buffer
